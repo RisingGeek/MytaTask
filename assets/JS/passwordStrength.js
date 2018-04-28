f6942204aac891235aced4290c0f5656a169562c
RisingGeek <bhavaypuri15@gmail.com>
2018-04-28 12:09:21 +0530

    password strength indicator added

---
+++ assets/JS/passwordStrength.js
@@ -0,0 +1,18 @@
+function passwordStrength() {
+    var passwordStrength = document.getElementById('wrong');
+    document.getElementById('password').addEventListener('keyup', function() {
+        var password = document.getElementById('password').value;
+            if(password.length >=0 && password.length <=6) {
+                passwordStrength.style.color='red';
+                passwordStrength.innerHTML='Password strength : Weak';
+            }
+            else if(password.length >=7 && password.length <=13) {
+                passwordStrength.style.color='yellow';
+                passwordStrength.innerHTML='Password strength : Medium';
+            }
+            else if(password.length >13) {
+                passwordStrength.style.color='green';
+                passwordStrength.innerHTML='Password strength : Strong';
+            }
+    });
+}
